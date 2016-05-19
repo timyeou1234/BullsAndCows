255a830ac0571db40db52040daea2c45ceaa83c6
Brian Hu <billy170916@gmail.com>
2016-05-19 21:52:05 +0800

    prototype of the game

---
+++ BullsAndCows/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  BullsAndCows
+//
+//  Created by Brian Hu on 5/19/16.
+//  Copyright © 2016 Brian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBOutlet weak var guessButton: UIButton!
+    @IBOutlet weak var remainingTimeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var answearLabel: UILabel!
+    var remainingTime: UInt8! {
+        didSet {
+            remainingTimeLabel.text = "remaining time: \(remainingTime)"
+            if remainingTime == 0 {
+                guessButton.enabled = false
+            } else {
+                guessButton.enabled = true
+            }
+        }
+    }
+    
+    var hintArray = [(guess: String, hint: String)]() {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
+    // TODO: 1. decide the data type you want to use to store the answear
+    var answear: UInt16!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setGame()
+        generateAnswear()
+    }
+
+    func setGame() {
+        remainingTime = 9
+        hintArray.removeAll()
+        answearLabel.text = nil
+        guessTextField.text = nil
+    }
+    
+    func generateAnswear() {
+        // TODO: 2. generate your answear here
+    }
+    
+    @IBAction func guess(sender: AnyObject) {
+        let guessString = guessTextField.text
+        guard guessString?.characters.count == 4 else {
+            let alert = UIAlertController(title: "you should input 4 digits to guess!", message: nil, preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        // TODO: 3. cast the guessString to the data type you want to use and judge the guess
+        
+        // TODO: 4. update the hint
+        let hint = "1A2B"
+        
+        hintArray.append((guessString!, hint))
+        
+        // TODO: 5. update correct
+        let correct = false
+        if correct {
+            let alert = UIAlertController(title: "Wow! You are awesome!", message: nil, preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            guessButton.enabled = false
+        } else {
+            remainingTime! -= 1
+        }
+    }
+    @IBAction func showAnswear(sender: AnyObject) {
+        // TODO: 6. cast your answear to string and display it if it's necessary
+        answearLabel.text = "\(answear)"
+    }
+    
+    @IBAction func playAgain(sender: AnyObject) {
+        setGame()
+    }
+    
+    // MARK: TableView
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return hintArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Hint Cell", forIndexPath: indexPath)
+        let (guess, hint) = hintArray[indexPath.row]
+        cell.textLabel?.text = "\(guess) => \(hint)"
+        return cell
+    }
+}
+
